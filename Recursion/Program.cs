a308850ada1600c84c118cb72f6b255c582dbfcb
nikolayGameDevGO <ninileo55555@gmail.com>
2023-03-30 16:13:01 +0500

    +sript recursion

---
+++ Recursion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Recursion
+{
+    internal class Program
+    {
+        private static int total;
+        public static void Main()
+        {
+            List<int> numbers = new List<int> { 1, 2, 4 };
+            Sum(numbers);
+        }
+
+        public static int Sum(List<int> numbers)
+        {
+            if (numbers.Count == 1)
+            {
+                total += numbers[0];
+                Console.WriteLine(total);
+                return total;
+            }
+            else
+            {
+                total += numbers[numbers.Count - 1];
+                if (numbers.Count != 1)
+                {
+                    numbers.RemoveAt(numbers.Count - 1);
+                }
+                Sum(numbers);
+
+                return total;
+            }
+        }
+    }
+}
