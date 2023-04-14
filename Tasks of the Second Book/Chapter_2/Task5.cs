64a5fd1005d224f83bfe0ebb745f061f634c1b0a
nikolayGameDevGO <ninileo55555@gmail.com>
2023-04-14 19:38:02 +0500

    +tasks

---
+++ Tasks of the Second Book/Chapter_2/Task5.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks_of_the_Second_Book.Chapter_2
+{
+    delegate int MyDelegate5();
+    internal class Task5
+    {
+
+    }
+}
--- Tasks of the Second Book/Main.cs
+++ Tasks of the Second Book/Main.cs
@@ -1,13 +1,15 @@
-﻿using Tasks_of_the_Second_Book.Chapter_2;
+﻿
+
+using Tasks_of_the_Second_Book.Chapter_2;
 
 namespace Tasks_of_the_Second_Book
 {
     internal class MainClass
     {
-        private static void Main()
+        public static void Main()
         {
-            Task3 task3 = new Task3("end", false);
-            task3.Delegate();
+           
+
         }
     }
 
