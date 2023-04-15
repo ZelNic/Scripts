29ba9b1f1ed923fe0b54a1cf90d4338d7201d38e
nikolayGameDevGO <ninileo55555@gmail.com>
2023-04-15 13:44:40 +0500

    +

---
+++ Tasks of the Second Book/Chapter_2/Task9.cs
@@ -0,0 +1,11 @@
+﻿namespace Tasks_of_the_Second_Book.Chapter_2
+{
+    internal delegate double MyDelegate9(double x);
+    internal class Task9
+    {
+        public static MyDelegate9 Quadratic(double a, double b, double c)
+        {
+            return (double x) => a * x + x * b + x * c;
+        }
+    }
+}
--- Tasks of the Second Book/Main.cs
+++ Tasks of the Second Book/Main.cs
@@ -1,11 +1,12 @@
-﻿namespace Tasks_of_the_Second_Book
+﻿using Tasks_of_the_Second_Book.Chapter_2;
+
+namespace Tasks_of_the_Second_Book
 {
     internal class MainClass
     {
         public static void Main()
         {
-
-
+                       
         }
 
     }
