e090820473ea01fab4141aa45632de71b7d31220
nikolayGameDevGO <ninileo55555@gmail.com>
2023-06-15 10:13:21 +0500

    attempt 605

---
+++ 1431/1431/605. Can Place Flowers.cs
@@ -0,0 +1,33 @@
+﻿namespace _1431
+{
+    public class _1431
+    {
+        public bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            for (int i = 0; i < flowerbed.Length; i++)
+            {
+                if (flowerbed[0] == 0)
+                {
+                    if ((flowerbed[i] == 0) && (flowerbed[i + 1] == 0))
+                    {
+                        if (n == 0)
+                        {
+                            return true;
+                        }
+                        n--;
+                    }
+                    else return false;
+                }
+                else if (flowerbed[0] == 1)
+                {
+                    if ((flowerbed[i] == 1) )
+                    {
+                        continue''
+                    }
+                }                
+            }
+            return true;
+        }
+    }
+
+}
