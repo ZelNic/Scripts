f0446d7ec6a88144df21c2abda9b117d01615def
Nikolay <128342772+ZelNic@users.noreply.github.com>
2023-06-27 09:40:47 +0500

    392 done

--- LeetCode/392. Is Subsequence.cs
+++ LeetCode/392. Is Subsequence.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
 
 namespace LeetCode
 {
@@ -6,21 +7,39 @@ namespace LeetCode
     {
         public static bool IsSubsequence(string s, string t)
         {
+            int[] num = new int[s.Length];
             string tmp = "";
             for (int i = 0; i < s.Length; i++)
-            {
-                for (int j = i; j < t.Length; j++)
-                {
+                for (int j = 0; j < t.Length; j++)
                     if (s[i] == t[j])
                     {
-                        tmp += t[j];
+                        if (i == 0)
+                        {
+                            tmp += t[j];
+                            num[i] = j;
+                            break;
+                        }
+                        else
+                            for (int k = 0; k < num.Length; k++)
+                            {
+                                if ((num[k] > j) || (num[k] == j))
+                                    break;
+
+
+                                if (k + 1 >= num.Length)
+                                {
+                                    tmp += t[j];
+                                    num[i] = j;
+                                    j = t.Length;
+                                    break;
+                                }
+                            }
+
                     }
-                }
-            }
+
             if (tmp == s)
-            {
                 return true;
-            }
+
             else return false;
         }
     }
