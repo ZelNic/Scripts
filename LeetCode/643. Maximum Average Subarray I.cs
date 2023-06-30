47d3bfbe2b32a959d568c91e61c4577e86dfac86
Nikolay <128342772+ZelNic@users.noreply.github.com>
2023-06-30 11:29:48 +0500

    solved 643

--- LeetCode/643. Maximum Average Subarray I.cs
+++ LeetCode/643. Maximum Average Subarray I.cs
@@ -8,33 +8,73 @@ namespace LeetCode
     {
         public static double FindMaxAverage(int[] nums, int k)
         {
+            double sum = 0;
             double sumAve = 0;
             double max = 0;
+            int startPoint = 0;
+            int endPoint = 0;
 
             for (int i = 0; i < nums.Length; i++)
             {
-                for (int j = i; j < i + k; j++)
-                {
-                    if (j < nums.Length)
+                if (i == 0)
+                    for (int j = i; j < i + k; j++)
                     {
-                        sumAve += nums[j];
+                        sum += nums[j];
 
                         if (j + 1 >= i + k)
                         {
-                            sumAve /= k;
+                            endPoint = j;
+                            sumAve = sum;
 
-                            if (i == 0)                            
-                                max = sumAve;
-                            
-                            else if (sumAve > max)                            
-                                max = sumAve;
-                            
-                            sumAve = 0;
+                            sumAve /= k;
+                            max = sumAve;
+
                         }
                     }
+                else
+                {
+                    if (endPoint + 1 < nums.Length)
+                    {
+                        endPoint++;
+                        sum -= nums[startPoint];
+                        sum += nums[endPoint];
+                        sumAve = sum;
+
+                        startPoint++;
+
+                        if (endPoint + 1 < nums.Length)
+                            endPoint++;
+
+                        sumAve /= k;
+
+                    }
+
+
+                    if (sumAve > max)
+                        max = sumAve;
+
                 }
             }
             return max;
         }
     }
 }
+
+
+//if (j < nums.Length)
+//{
+//    sumAve += nums[j];
+
+//    if (j + 1 >= i + k)
+//    {
+//        sumAve /= k;
+
+//        if (i == 0)
+//            max = sumAve;
+
+//        else if (sumAve > max)
+//            max = sumAve;
+
+//        sumAve = 0;
+//    }
+//}
